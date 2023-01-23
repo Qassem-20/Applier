6b61db9dff2863f9fc12b773f5ca4cd2cee85594
Qassem <55999417+Qassem-20@users.noreply.github.com>
2023-01-23 19:44:54 +0300

    change language (still not working properly)

--- src/components/homePage/about.jsx
+++ src/components/homePage/about.jsx
@@ -1,11 +1,14 @@
 import '../../assets/css/welcoming.css'
 import React, { Component } from 'react'
+import { Container } from 'react-bootstrap'
 
 export class about extends Component {
   render() {
     return (
       <div className='container-about' id='About'>
       <h4>About</h4>
+        <p>change your language</p>
+        <div id="google_translate_element">  </div>
       </div>  
     )
   }
