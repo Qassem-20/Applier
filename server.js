669153946bf16b69663849fcb70df731c284577e
Qassem <55999417+Qassem-20@users.noreply.github.com>
2023-01-30 12:43:23 +0300

    establish DB connection
    - using mongoose atlas

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './db/connect.js';
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Welcome!');
 });
 
+// assign a port for the server
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+// DB connection to mongoose atlas
+const start = async () =>{
+  try{
+    await connectDB(/*the URL in env file*/process.env.MONGO_URL)
+      app.listen(port, () => {
+        console.log(`Server is listening on port ${port}..., and Data base is connected`);
+      });
+    } catch (error) {
+      console.log(error)
+  }
+}
 
-
+start();
