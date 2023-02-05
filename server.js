567ab154ce675064bd5e85010a27e34b463b4c53
Qassem <55999417+Qassem-20@users.noreply.github.com>
2023-02-05 17:35:06 +0300

    adding proxy

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/api/v1/panel',adminPanelRouter)
 // assign a port for the server
 const port = process.env.PORT || 4000;
 
-/* connection to the front end
+//connection to the front end
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import path from 'path';
@@ -47,7 +47,7 @@ app.use(express.static(path.resolve(__dirname, './client/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
-*/
+
 //middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
