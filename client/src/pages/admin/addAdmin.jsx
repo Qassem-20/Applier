050b58cd209f0d7879b015e6121cb545191321a6
Qassem <55999417+Qassem-20@users.noreply.github.com>
2023-02-21 10:16:10 +0300

    Update addAdmin.jsx

--- client/src/pages/admin/addAdmin.jsx
+++ client/src/pages/admin/addAdmin.jsx
@@ -8,7 +8,7 @@ const AddAdmin = () => {
 
 
 
-    /*use state
+   //use state
     const [name,setName] = useState("")
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
@@ -33,7 +33,7 @@ const AddAdmin = () => {
       const data = await response.json();
         console.log("Account is registered", data);
     }
-    */
+
   return (
     <Fragment>
     <AdminNav />
