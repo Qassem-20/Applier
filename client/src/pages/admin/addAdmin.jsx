a8f6df8597679319bffab976602d93ab98344b76
Qassem <55999417+Qassem-20@users.noreply.github.com>
2023-02-28 16:32:17 +0300

    addAdmin, delete Admin

--- client/src/pages/admin/addAdmin.jsx
+++ client/src/pages/admin/addAdmin.jsx
@@ -1,12 +1,12 @@
-import '../../assets/css/admin.css';
-import AdminNav from '../../components/Nav/adminNav';
-import React, { Fragment, useState, useEffect } from 'react'
-import {Container, Row, Col} from 'react-bootstrap';
-import { useAppContext } from '../../context/appContext.js';
-import axios from 'axios';
+import "../../assets/css/admin.css";
+import AdminNav from "../../components/Nav/adminNav";
+import React, { Fragment, useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { useAppContext } from "../../context/appContext.js";
+import axios from "axios";
 
 const AddAdmin = () => {
-   /*use state
+  /*use state
     const [name,setName] = useState("")
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
@@ -56,56 +56,95 @@ const AddAdmin = () => {
     };
     */
 
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    type: "",
+    phone: "",
+    password: "",
+  });
 
-    const [values, setValues] = useState({
-      name:'',
-      email:'',
-      type:'',
-      phone:'',
-      password:'',
-    })
-
-    //to view the enabled value
-    const handleChange = (e) => {
-      const {name, value} = e.target;
-      setValues({
-        ...setValues,
-        [name]:value
-      })
-    }
-    const registerAdmin = async (e) => {
-      e.preventDefault();
-      const res = await axios.post("http://localhost:4000/api/v1/admins/registerAdmin", setValues);
-      console.log(res)
-    }
+  //to view the enabled value
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+  const registerAdmin = async (e) => {
+    e.preventDefault();
+    // add admin
+    const res = await axios.post(
+      "http://localhost:4000/api/v1/admins/registerAdmin",
+      values
+    );
+    console.log(res);
+  };
   return (
     <Fragment>
-    <AdminNav />
-    <Container className='bg-white rounded p-3' >
-      <Row>
-        <Col>
-        <form onSubmit={registerAdmin}>
-            <p className='mb-1'>Name:</p>
-            <input className='inputStyling' type="text" placeholder='Name' value={values.name} onChange={handleChange}/>
-            <p className='mb-1'>Email:</p>
-            <input className='inputStyling' type="email" placeholder='Email' value={values.email} onChange={handleChange}/>
-            <p className='mb-1'>Phone Number:</p>
-            <input className='inputStyling' type="number" placeholder='Phone number' value={values.phone} onChange={handleChange}/>
-            <p className='mb-1'>Password:</p>
-            <input className='inputStyling' type="password" placeholder='Password' value={values.password} onChange={handleChange}/>
-            <p className='mb-1'>Type of the admin:</p>
-            <select className='inputStyling' name="type" placeholder='type' value={values.type} onChange={handleChange}>
-              <option disabled>type</option>
-              <option value="sub-admin">sub-admin</option>
-              <option value="main-admin">main-admin</option>
-            </select>
-            <button type='submit' className='btn login'>submit</button>
-          </form>
-        </Col>
-      </Row>
-    </Container>
-  </Fragment>
-  )
-}
+      <AdminNav />
+      <Container className="bg-white rounded p-3">
+        <Row>
+          <Col>
+            <form onSubmit={registerAdmin}>
+              <p className="mb-1">Name:</p>
+              <input
+                className="inputStyling"
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={values.name}
+                onChange={handleChange}
+              />
+              <p className="mb-1">Email:</p>
+              <input
+                className="inputStyling"
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+              />
+              <p className="mb-1">Phone Number:</p>
+              <input
+                className="inputStyling"
+                type="number"
+                name="phone"
+                placeholder="Phone number"
+                value={values.phone}
+                onChange={handleChange}
+              />
+              <p className="mb-1">Password:</p>
+              <input
+                className="inputStyling"
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={values.password}
+                onChange={handleChange}
+              />
+              <p className="mb-1">Type of the admin:</p>
+              <select
+                className="inputStyling"
+                name="type"
+                placeholder="type"
+                value={values.type}
+                onChange={handleChange}
+              >
+                <option disabled>type</option>
+                <option value="sub-admin">sub-admin</option>
+                <option value="main-admin">main-admin</option>
+              </select>
+              <button type="submit" className="btn login">
+                submit
+              </button>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </Fragment>
+  );
+};
 
-export default AddAdmin
+export default AddAdmin;
--- client/src/pages/admin/adminPanel.jsx
+++ client/src/pages/admin/adminPanel.jsx
@@ -1,59 +1,121 @@
-import '../../assets/css/admin.css';
-import AdminNav from '../../components/Nav/adminNav';
-import {Container, Row, Col} from 'react-bootstrap';
+import "../../assets/css/admin.css";
+import AdminNav from "../../components/Nav/adminNav";
+import { Container, Row, Col } from "react-bootstrap";
 import adminsStore from "../../stores/AdminsStore.js";
-import React, { Fragment, useState, useEffect } from 'react'
-import axios from 'axios';
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 
 const AdminPanel = () => {
   const store = adminsStore();
 
-  const [admins, setAdmins] = useState(null)
+  //show Admins
+  const [admins, showAdmins] = useState(null);
+
+  //update Admins
+  const [updateType, setUpdateType] = useState({
+    _id: null,
+    type: "",
+  });
 
   useEffect(() => {
     fetchAdmins();
-  },[])
+  }, []);
 
   const fetchAdmins = async () => {
     const res = await axios.get("http://localhost:4000/api/v1/admins");
     // Set to state
-    setAdmins(res.data.admins);
-  }
+    showAdmins(res.data.admins);
+  };
 
+  const deleteAdmin = async (_id) => {
+    const res = await axios.delete(
+      "http://localhost:4000/api/v1/admins/" + _id
+    );
+    //update page;
+    const newAdmins = [...admins].filter((admin) => {
+      return admin._id !== _id;
+    });
+    showAdmins(newAdmins);
+  };
+
+  const updateAdminType = (e) => {
+    const { value, name } = e.target;
+    setUpdateType({
+      ...updateType,
+      [name]: value,
+    });
+  };
 
   return (
-<Fragment>
-        <AdminNav />
-        <Container className='mb-2'>
-            <Row>
-              <Col md={6} xs={12}><h1>Admin Panel</h1></Col>
-              <Col md={6} xs={12}><a className='btn btn-dark' href="/addAdmin">add new admin</a></Col>
-            </Row>
-        </Container>
-        <Container fluid>
-            <Row className='opportunitiesTag'>
-            <Col xl={3} md={2} xs={3}><p className='opportunitiesMainTags'>Name</p></Col>
-            <Col xl={3} md={3} xs={3}><p className='opportunitiesMainTags'>Email</p></Col>
-            <Col xl={3} md={3} xs={2}><p className='opportunitiesMainTags'>Phone Number</p></Col>
-            <Col xl={1} md={1} xs={1}><p className='opportunitiesMainTags'>type</p></Col>
-            <Col xl={2} md={1} xs={1}><p className='opportunitiesMainTags'>Status</p></Col>
-            </Row>
-        </Container>
-        {admins && 
+    <Fragment>
+      <AdminNav />
+      <Container className="mb-2">
+        <Row>
+          <Col md={6} xs={12}>
+            <h1>Admin Panel</h1>
+          </Col>
+          <Col md={6} xs={12}>
+            <a className="btn btn-dark" href="/addAdmin">
+              add new admin
+            </a>
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid>
+        <Row className="opportunitiesTag">
+          <Col xl={3} md={2} xs={3}>
+            <p className="opportunitiesMainTags">Name</p>
+          </Col>
+          <Col xl={3} md={3} xs={3}>
+            <p className="opportunitiesMainTags">Email</p>
+          </Col>
+          <Col xl={3} md={3} xs={2}>
+            <p className="opportunitiesMainTags">Phone Number</p>
+          </Col>
+          <Col xl={1} md={1} xs={1}>
+            <p className="opportunitiesMainTags">type</p>
+          </Col>
+          <Col xl={2} md={1} xs={1}>
+            <p className="opportunitiesMainTags">Status</p>
+          </Col>
+        </Row>
+      </Container>
+      {admins &&
         admins.map((admin) => {
-          return <Container fluid key={admin._id} >
-              <Row className='opportunitiesT'>
-                <Col xl={3} md={2} xs={3}><p className='opportunitiesTags'>{admin.name}</p></Col>
-                <Col xl={3} md={3} xs={3}><p className='opportunitiesTags'>{admin.email}</p></Col>
-                <Col xl={3} md={3} xs={2}><p className='opportunitiesTags'>{admin.phone}</p></Col>
-                <Col xl={2} md={1} xs={1}><button className='opportunitiesTags'>{admin.type}</button></Col>
-                <Col xl={1} md={1} xs={1} ><input className='opportunitiesTags' type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off" /><label class="btn btn-outline-danger" for="btn-check-outlined">Delete</label>
+          return (
+            <Container fluid key={admin._id}>
+              <Row className="opportunitiesT">
+                <Col xl={3} md={2} xs={3}>
+                  <p className="opportunitiesTags">{admin.name}</p>
+                </Col>
+                <Col xl={3} md={3} xs={3}>
+                  <p className="opportunitiesTags">{admin.email}</p>
+                </Col>
+                <Col xl={3} md={3} xs={2}>
+                  <p className="opportunitiesTags">{admin.phone}</p>
+                </Col>
+                <Col xl={2} md={1} xs={1}>
+                  <input
+                    type="text"
+                    value={admin.type}
+                    onChange={updateAdminType}
+                  />
+                </Col>
+                <Col xl={1} md={1} xs={1}>
+                  <button
+                    ml-0
+                    className="deleteBtn"
+                    onClick={() => deleteAdmin(admin._id)}
+                  >
+                    Delete
+                  </button>
                 </Col>
               </Row>
-          </Container>
+            </Container>
+          );
         })}
-      </Fragment>
-  )
-}
+    </Fragment>
+  );
+};
 
-export default AdminPanel
+export default AdminPanel;
