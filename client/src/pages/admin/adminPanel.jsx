fe068df37142050e48f335c4433886f04c88e2c8
Qassem <55999417+Qassem-20@users.noreply.github.com>
2023-03-05 19:40:37 +0300

    trying to fetch update{PUT}

--- client/src/pages/admin/adminPanel.jsx
+++ client/src/pages/admin/adminPanel.jsx
@@ -7,8 +7,11 @@ import React, { Fragment, useEffect } from "react";
 const AdminPanel = () => {
   const store = adminsStore();
 
-  const storeDelete = adminsStore((storeDelete) => {
-    return { deleteAdmin: storeDelete.deleteAdmin };
+  const storeDeleteAndUpdate = adminsStore((storeDeleteAndUpdate) => {
+    return {
+      deleteAdmin: storeDeleteAndUpdate.deleteAdmin,
+      toggleUpdate: storeDeleteAndUpdate.toggleUpdate,
+    };
   });
 
   /* this to get current value int tags (input)
@@ -75,8 +78,20 @@ const AdminPanel = () => {
                 <Col xl={2} md={1} xs={1}>
                   <form onSubmit={store.updateAdminType}>
                     <select name="type" defaultValue={admin.type}>
-                      <option value="sub-admin">sub-admin</option>
-                      <option value="main-admin">main-admin</option>
+                      <option
+                        value={store.updateAdminType.type}
+                        onChange={store.handleUpdateFieldChange}
+                        name="type"
+                      >
+                        sub-admin
+                      </option>
+                      <option
+                        value={store.updateAdminType.type}
+                        onChange={store.handleUpdateFieldChange}
+                        name="type"
+                      >
+                        main-admin
+                      </option>
                     </select>
                     <button type="submit">.</button>
                   </form>
@@ -84,7 +99,7 @@ const AdminPanel = () => {
                 <Col xl={1} md={1} xs={1}>
                   <button
                     className="deleteBtn"
-                    onClick={() => storeDelete.deleteAdmin(admin._id)}
+                    onClick={() => storeDeleteAndUpdate.deleteAdmin(admin._id)}
                   >
                     Delete
                   </button>
--- client/src/stores/AdminsStore.js
+++ client/src/stores/AdminsStore.js
@@ -1,64 +1,110 @@
-import { create } from 'zustand';
-import axios from 'axios';
+import { create } from "zustand";
+import axios from "axios";
 
 const AdminsStore = create((set) => ({
-    admins: null,
+  admins: null,
 
-    fetchAdmins: async () => {
-        // Fetch the admins
-        const res = await axios.get("http://localhost:4000/api/v1/admins",{withCredentials:true});
-        // Set to state
-        set({admins: res.data.admins});
-      }, 
+  fetchAdmins: async () => {
+    // Fetch the admins
+    const res = await axios.get("http://localhost:4000/api/v1/admins", {
+      withCredentials: true,
+    });
+    // Set to state
+    set({ admins: res.data.admins });
+  },
 
-      deleteAdmin: async (_id) => {
-        const res = await axios.delete(
-          "http://localhost:4000/api/v1/admins/" + _id,{withCredentials:true}
-        );
+  deleteAdmin: async (_id) => {
+    const res = await axios.delete(
+      "http://localhost:4000/api/v1/admins/" + _id,
+      { withCredentials: true }
+    );
 
-        const { admins } = AdminsStore.getState();
+    const { admins } = AdminsStore.getState();
 
-        //update page;
-        const newAdmins = [...admins].filter((admin) => {
-          return admin._id !== _id;
-        });
-        set({ admins: newAdmins });
+    //update page;
+    const newAdmins = [...admins].filter((admin) => {
+      return admin._id !== _id;
+    });
+    set({ admins: newAdmins });
+  },
+
+  updateType: {
+    _id: "",
+    type: "",
+  },
+  handleUpdateFieldChange: (e) => {
+    const { value, name } = e.target;
+
+    set((state) => {
+      return {
+        updateType: {
+          ...state.updateType,
+          [name]: value,
+        },
+      };
+    });
+  },
+
+  toggleUpdate: ({ _id, type }) => {
+    set({
+      updateType: {
+        _id,
+        type,
       },
+    });
+  },
 
+  updateAdminType: async (e) => {
+    e.preventDefault();
+
+    const {
+      updateType: { type, _id },
+      admins,
+    } = AdminsStore.getState();
+
+    // Send the update request
+    const res = await axios.put(
+      `http://localhost:4000/api/v1/admins/${_id}`,
+      {
+        type,
+      },
+      { withCredentials: true }
+    );
+
+    // Update state
+    const newAdmins = [...admins];
+    const adminIndex = admins.findIndex((admin) => {
+      return admin._id === _id;
+    });
+    newAdmins[adminIndex] = res.data.admin;
+
+    set({
+      admins: newAdmins,
       updateType: {
         _id: null,
         type: "",
       },
+    });
+  },
 
-      updateAdminType: async (e) => {
-        e.preventDefault();
+  values: {
+    name: "",
+    email: "",
+    type: "",
+    phone: "",
+    password: "",
+  },
 
-        const {
-          updateType: { type, _id },
-          admins,
-        } = AdminsStore.getState();
-    
-        // Send the update request
-        const res = await axios.put(`http://localhost:4000/api/v1/admins/${_id}`, {
-          type,
-        },{withCredentials:true});
-    
-        // Update state
-        const newAdmins = [...admins];
-        const adminIndex = admins.findIndex((admin) => {
-          return admin._id === _id;
-        });
-        newAdmins[adminIndex] = res.data.admin;
-    
-        set({
-          admins: newAdmins,
-          updateType: {
-            _id: null,
-            type: "",
-          },
-        });
-      },
+  registerAdmin: async () => {
+    const { values } = AdminsStore.getState();
 
+    // add admin
+    const res = await axios.post(
+      "http://localhost:4000/api/v1/registerAdmin",
+      values,
+      { withCredentials: true }
+    );
+    set({
       values: {
         name: "",
         email: "",
@@ -66,79 +112,65 @@ const AdminsStore = create((set) => ({
         phone: "",
         password: "",
       },
+    });
+  },
 
-      registerAdmin: async () => {
-        const { values } = AdminsStore.getState();
+  handleChange: async (e) => {
+    const { name, value } = e.target;
 
-        // add admin
-        const res = await axios.post(
-            "http://localhost:4000/api/v1/registerAdmin",
-          values,{withCredentials:true}
-        );
-        set({values: {
-          name: "",
-          email: "",
-          type: "",
-          phone: "",
-          password: "",
-        },})
-      },
+    set((state) => {
+      return {
+        values: {
+          ...state.values,
+          [name]: value,
+        },
+      };
+    });
+  },
 
-      handleChange: async (e) =>{
-        const { name, value } = e.target;
+  //login
+  loggedIn: null,
+  loginForm: {
+    email: "",
+    password: "",
+  },
+  handleChangeLogin: async (e) => {
+    const { name, value } = e.target;
 
-        set((state)=>{
-          return{
-            values:{
-              ...state.values,
-              [name]:value,
-            }
-          }
-        })
-      },
+    set((state) => {
+      return {
+        loginForm: {
+          ...state.loginForm,
+          [name]: value,
+        },
+      };
+    });
+  },
+  loginAdmin: async () => {
+    const { loginForm } = AdminsStore.getState();
 
-      //login
-      loggedIn:null,
-      loginForm:{
-        email:"",
-        password:"",
-      },
-      handleChangeLogin: async (e) =>{
-        const { name, value } = e.target;
+    await axios.post("http://localhost:4000/api/v1/loginAdmin", loginForm, {
+      withCredentials: true,
+    });
 
-        set((state)=>{
-          return{
-            loginForm:{
-              ...state.loginForm,
-              [name]:value,
-            }
-          }
-        })
-      },
-      loginAdmin: async () =>{
-        const {loginForm} = AdminsStore.getState();
-
-        await axios.post("http://localhost:4000/api/v1/loginAdmin",loginForm, {
-          withCredentials:true,
-        });
-
-        set({loggedIn:true})
-
-      },
-      checkAuth: async () =>{
-        try{
-        await axios.get("http://localhost:4000/api/v1/checkAuthAdmin", {
-          withCredentials:true,
-        });
-        set({loggedIn:true})
-      }catch(err){
-        set({loggedIn:false})
-      }
-      },
-      logout: async () =>{
-        await axios.get("http://localhost:4000/api/v1/logutAdmin",{withCredentials:true})
-        set({loggedIn:false})
-      },
-}))
+    set({ loggedIn: true });
+  },
+  checkAuth: async () => {
+    try {
+      await axios.get("http://localhost:4000/api/v1/checkAuthAdmin", {
+        withCredentials: true,
+      });
+      set({ loggedIn: true });
+    } catch (err) {
+      set({ loggedIn: false });
+    }
+  },
+  logout: async () => {
+    await axios.get("http://localhost:4000/api/v1/logutAdmin", {
+      withCredentials: true,
+    });
+    set({ loggedIn: false });
+  },
+}));
 
 export default AdminsStore;
