d8ee0371bd27b873a60fe11bf8df8007b953ad21
Qassem <55999417+Qassem-20@users.noreply.github.com>
2023-04-09 18:47:41 +0300

    Update feedBackCompany.jsx

--- client/src/pages/company/feedBackCompany.jsx
+++ client/src/pages/company/feedBackCompany.jsx
@@ -8,13 +8,19 @@ import CompanyNav from "../../components/Nav/companyNav";
 import axios from "axios";
 
 const FeedBackCompany = () => {
-  const [reviews, setReviews] = useState([]);
+  const [reviews, setReviews] = useState([{ isReported: "" }]);
 
+  function handleUserDataChange(event) {
+    setReviews({
+      ...reviews,
+      isReported: event.target.value,
+    });
+  }
   useEffect(() => {
     const fetchReviews = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/reviewsCompany",
+          "http://localhost:4000/api/v1/companyReviews",
           {
             withCredentials: true,
           }
@@ -29,13 +35,17 @@ const FeedBackCompany = () => {
     fetchReviews();
   }, []);
 
-  const handleReport = (reportedReview) => {
-    axios.put(
-      `http://localhost:4000/api/v1/reportReviewCompany/:${reportedReview}`,
-      {
-        withCredentials: true,
-      }
-    );
+  const handleReport = (_id) => {
+    try {
+      const response = axios.put(
+        `http://localhost:4000/api/v1/reportReviewCompany/${_id}`,
+        reviews,
+        { withCredentials: true }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -56,9 +66,12 @@ const FeedBackCompany = () => {
           </Col>
         </Row>
       </Container>
-      <Container className="mt-4 rounded bg-white border border-dark p-2">
-        {reviews.map((review) => (
-          <Row key={review._id} className="rounded border-bottom p-5">
+      {reviews.map((review) => (
+        <Container
+          className="mt-4 rounded bg-white border border-dark p-2"
+          key={review._id}
+        >
+          <Row className="rounded p-5">
             <Col sm={3}>
               <img className="m-auto" src={profileImg} alt="" />
             </Col>
@@ -66,17 +79,24 @@ const FeedBackCompany = () => {
               <p className="pt-5">{review.description}</p>
             </Col>
             <Col className="m-auto" sm={2}>
-              <a href="" onClick={handleReport(review)}>
-                <img className="infoImg" src={flagIcon} alt="Report Icon" />
-              </a>
-
+              <div>
+                <select
+                  name="isReported"
+                  defaultValue={review.isReported}
+                  onChange={handleUserDataChange}
+                  onClick={() => handleReport(review._id)}
+                >
+                  <option value="no">not Reported</option>
+                  <option value="yes">Report</option>
+                </select>
+              </div>
               <span>
                 {review.rate} <span>&#11088;</span>
               </span>
             </Col>
           </Row>
-        ))}
-      </Container>
+        </Container>
+      ))}
     </Fragment>
   );
 };
