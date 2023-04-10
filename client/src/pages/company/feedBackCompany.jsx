2050ceeb48ab93944ef131f6320cf4e07ea64162
Qassem <55999417+Qassem-20@users.noreply.github.com>
2023-04-11 01:14:00 +0300

    now you can report a review

--- client/src/pages/company/feedBackCompany.jsx
+++ client/src/pages/company/feedBackCompany.jsx
@@ -8,11 +8,13 @@ import CompanyNav from "../../components/Nav/companyNav";
 import axios from "axios";
 
 const FeedBackCompany = () => {
-  const [reviews, setReviews] = useState([{ isReported: "" }]);
+  const [reviews, setReviews] = useState([]);
+
+  const [userData, setUserData] = useState({ isReported: "" });
 
   function handleUserDataChange(event) {
-    setReviews({
-      ...reviews,
+    setUserData({
+      ...userData,
       isReported: event.target.value,
     });
   }
@@ -38,8 +40,8 @@ const FeedBackCompany = () => {
   const handleReport = (_id) => {
     try {
       const response = axios.put(
-        `http://localhost:4000/api/v1/reportReviewCompany/${_id}`,
-        reviews,
+        `http://localhost:4000/api/v1/companies/reportReview/${_id}`,
+        userData,
         { withCredentials: true }
       );
       console.log(response.data);
@@ -72,9 +74,6 @@ const FeedBackCompany = () => {
           key={review._id}
         >
           <Row className="rounded p-5">
-            <Col sm={3}>
-              <img className="m-auto" src={profileImg} alt="" />
-            </Col>
             <Col sm={7}>
               <p className="pt-5">{review.description}</p>
             </Col>
@@ -86,8 +85,8 @@ const FeedBackCompany = () => {
                   onChange={handleUserDataChange}
                   onClick={() => handleReport(review._id)}
                 >
-                  <option value="no">not Reported</option>
-                  <option value="yes">Report</option>
+                  <option value="no">Not Reported</option>
+                  <option value="yes">Reported</option>
                 </select>
               </div>
               <span>
