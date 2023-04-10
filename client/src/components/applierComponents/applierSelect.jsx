2f806efe4cd0d3d633f86631ea01265c3303c1a4
Bander Al-Mutairi <73838482+B6xb@users.noreply.github.com>
2023-04-11 01:10:55 +0300

    added react-select to the node_modules

--- client/src/components/applierComponents/applierSelect.jsx
+++ client/src/components/applierComponents/applierSelect.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "react-bootstrap";
 import "../../assets/css/signUpSignIn.css";
 import "../../assets/css/custom.css";
-import Select from 'react-select';
+import Select from "react-select";
 
 const styles = {
-  control: styles => ({ ...styles, backgroundColor: 'eaeaeb'}),
-  
-}
+  control: (styles) => ({ ...styles, backgroundColor: "eaeaeb" }),
+};
 
 const Checkbox = (props) => {
   const [selectedSkills, setSelectedSkills] = useState(null);
@@ -16,28 +15,23 @@ const Checkbox = (props) => {
     setSelectedSkills(selectedSkills);
   };
 
-    const {
-      name,
-      placeholder,
-      label,
-      options
-    } = props;
-  
-    return (
-      <div>
-        <label className="labelStyling font-bold">{label}</label>
-        <Select
-          className="inputStyling mb-3"
-          name={name}
-          value={selectedSkills}
-          onChange={handleSkillsChange}
-          placeholder={placeholder}
-          options={options}
-          isMulti
-          styles={styles}
-        />
-      </div>
-    );
-  };
+  const { name, placeholder, label, options } = props;
+
+  return (
+    <div>
+      <label className="labelStyling font-bold">{label}</label>
+      <Select
+        className="inputStyling mb-3"
+        name={name}
+        value={selectedSkills}
+        onChange={handleSkillsChange}
+        placeholder={placeholder}
+        options={options}
+        isMulti
+        styles={styles}
+      />
+    </div>
+  );
+};
 
 export default Checkbox;
