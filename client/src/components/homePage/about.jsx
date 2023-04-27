9c9d302311beff0c62493e1ff11ae4d592767c89
Turki Almalki <iiata767@gmail.com>
2023-04-27 08:40:00 +0300

    Created full functioning footer

--- client/src/components/homePage/about.jsx
+++ client/src/components/homePage/about.jsx
@@ -11,7 +11,40 @@ export class about extends Component {
         <div id="google_translate_element"> </div> */}
         <div className="sb_footer section_padding">
           <div className="sb_footer-Links">
-            
+            <div className="sb_footer-Links_div">
+              <h4>Company</h4>
+              <a href="/about">
+                <p>About</p>
+              </a>
+              <a href="/careers">
+                <p>Careers</p>
+              </a>
+              <a href="/contact">
+                <p>Contact Us</p>
+              </a>
+            </div>
+            <div className="sb_footer-Links_div">
+              <h4>Useful Links</h4>
+              <a href="/">
+                <p>Home</p>
+              </a>
+              <a href="/routesignup">
+                <p>Sign Up</p>
+              </a>
+              <a href="/signin">
+                <p>Sign In</p>
+              </a>
+            </div> 
+          </div>
+
+          <hr></hr>
+
+          <div className="sb_footer-below">
+            <div className="sb_footer-copyright">
+              <p>
+                @{new Date().getFullYear()} Applier, All Rights Reserved.
+              </p>
+            </div>
           </div>
         </div>
       </div>
