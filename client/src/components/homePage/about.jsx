c54c4b85b5493e57e3c76fcbca88c0b74ab98f7f
Turki Almalki <iiata767@gmail.com>
2023-04-27 08:12:13 +0300

    Created CSS for the home page footer

--- client/src/components/homePage/about.jsx
+++ client/src/components/homePage/about.jsx
@@ -1,13 +1,19 @@
 import "../../assets/css/welcoming.css";
+import "../../assets/css/footer.css"
 import React, { Component } from "react";
 
 export class about extends Component {
   render() {
     return (
       <div className="container-about" id="About">
-        <h4>About</h4>
+        {/* <h4>About</h4>
         <p>change your language</p>
-        <div id="google_translate_element"> </div>
+        <div id="google_translate_element"> </div> */}
+        <div className="sb_footer section_padding">
+          <div className="sb_footer-Links">
+            
+          </div>
+        </div>
       </div>
     );
   }
