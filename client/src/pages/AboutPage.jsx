70ffc99b1ec2e498245fb78a9749c205169c94aa
Turki Almalki <iiata767@gmail.com>
2023-04-28 19:53:13 +0300

    Finished About Us page

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import WelcomeNav from "../components/Nav/welcomeNav";
 import "../assets/css/welcoming.css";
 import { Container, Row, Col } from "react-bootstrap";
+import AboutUsImage from "../assets/images/AboutUs.jpg";
 
 const AboutPage = () => {
   return (
@@ -13,9 +14,11 @@ const AboutPage = () => {
       <h3 style={{lineHeight: 3}}>About Us</h3>
         <Row>
           <Col xl={6} md={6} sm={12}>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos rem assumenda sit, eius, vel nemo amet sed, explicabo animi fugit itaque nihil reprehenderit! Voluptas fugit ipsam atque, saepe quisquam officiis.
-            </p>
+            <p style={{textAlign: "justify"}}>
+            Welcome to our website, where we provide a platform for students seeking training opportunities to connect with potential companies and patients. Our website caters to a wide range of students, including medical students, who are looking to gain practical experience and enhance their skills in their respective fields. We aim to bridge the gap between students and companies by providing a user-friendly interface that allows students to browse and apply for training opportunities, while companies and patients can post their requirements and search for suitable candidates. Our mission is to facilitate the exchange of knowledge and skills between students and professionals, ultimately contributing to the development of the next generation of skilled professionals. Join us today and embark on a journey of growth and learning!            </p>
+          </Col>
+          <Col xl={6}>
+            <img src={AboutUsImage} alt="Application" className="AboutUsImgs" />
           </Col>
         </Row>
       </Container>
