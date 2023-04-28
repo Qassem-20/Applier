e1eacb3202f5876b8fcafa429055a34ea1aad330
Turki Almalki <iiata767@gmail.com>
2023-04-28 13:20:12 +0300

    Update about.jsx

--- client/src/components/homePage/about.jsx
+++ client/src/components/homePage/about.jsx
@@ -7,9 +7,6 @@ export class about extends Component {
   render() {
     return (
       <div className="container-about" id="About">
-        {/* <h4>About</h4>
-        <p>change your language</p>
-        <div id="google_translate_element"> </div> */}
         <div className="sb_footer section_padding container">
           <div className="sb_footer-Links">
             <div className="sb_footer-Links_div">
