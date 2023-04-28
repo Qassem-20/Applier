0916d282a4ad1c276a8f073fe2486086c2d5ccfc
Turki Almalki <iiata767@gmail.com>
2023-04-28 13:39:16 +0300

    Update AboutPage.jsx

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import WelcomeNav from "../components/Nav/welcomeNav";
+import "../assets/css/welcoming.css";
 
 const AboutPage = () => {
   return (
@@ -7,6 +8,12 @@ const AboutPage = () => {
       <WelcomeNav />
       <section>
       </section>
+      <div className="container">
+        <h3 style={{lineHeight: 3}}>About Us</h3>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos rem assumenda sit, eius, vel nemo amet sed, explicabo animi fugit itaque nihil reprehenderit! Voluptas fugit ipsam atque, saepe quisquam officiis.
+        </p>
+      </div>
     </Fragment>
   );
 };
