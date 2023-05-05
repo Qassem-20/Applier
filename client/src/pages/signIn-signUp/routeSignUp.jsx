9900c62c07b4fdf84af175bbc329e379aca4ad92
Turki Almalki <iiata767@gmail.com>
2023-05-05 21:54:45 +0300

    Updated routeSignUp page

--- client/src/pages/signIn-signUp/routeSignUp.jsx
+++ client/src/pages/signIn-signUp/routeSignUp.jsx
@@ -1,9 +1,9 @@
 import "../../assets/css/signUpSignIn.css";
-import traineeBackground from "../../assets/images/traineeBackground.png";
-import companyBackground from "../../assets/images/companyBackground.png";
-import medicalStudentBackground from "../../assets/images/medicalStudentBackground.jpeg";
+import traineeBackground from "../../assets/images/uniBackground.jpg";
+import companyBackground from "../../assets/images/companyBack.png";
+import medicalStudentBackground from "../../assets/images/medStudent.jpg";
 import WelcomeNav from "../../components/Nav/welcomeNav";
-import { Container, Row } from "react-bootstrap";
+import { Container, Stack, Card, Button } from "react-bootstrap";
 
 import React, { Component, Fragment } from "react";
 
@@ -14,32 +14,36 @@ export class routeSignUp extends Component {
         <WelcomeNav />
 
         <Container className="mt-2">
-          <Row>
-            <a className="col-4 align" href="/signUpConsumer">
-              <img
-                className="img-fluid"
-                src={traineeBackground}
-                alt="User img"
-              />
-              <h2 className="align">Universiy Student</h2>
-            </a>
-            <a className="col-4 align" href="/signUpMedicalStudent">
-              <img
-                className="img-fluid "
-                src={medicalStudentBackground}
-                alt="Medical img"
-              />
-              <h2 className="align">Medical Student</h2>
-            </a>
-            <a className="col-4 align" href="/signUpCompany">
-              <img
-                className="img-fluid"
-                src={companyBackground}
-                alt="Company img"
-              />
-              <h2 className="align">Company</h2>
-            </a>
-          </Row>
+          <Stack className="col-md-5 mx-auto">
+         
+          <Card style={{minHeight: 439}}>
+            <Card.Img variant="top" src={traineeBackground} />
+            <Card.Body>
+              <Card.Text>
+               <Button variant="dark" href="/signUpConsumer" style={{minWidth: 153}}>University Student</Button>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          
+          <Card style={{minHeight: 439}}>
+            <Card.Img variant="top" src={medicalStudentBackground} />
+            <Card.Body>
+              <Card.Text>
+               <Button variant="dark" href="/signUpMedicalStudent" style={{minWidth: 153}}>Medical Student</Button>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          
+          <Card style={{minHeight: 439}}>
+            <Card.Img variant="top" src={companyBackground} />
+            <Card.Body>
+              <Card.Text>
+               <Button variant="dark" href="signUpCompany" style={{minWidth: 153}}>Company</Button>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          
+          </Stack>
         </Container>
       </Fragment>
     );
