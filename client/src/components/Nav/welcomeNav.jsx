be74bc81b3a15bfb6eb64861275213dadcafe378
Turki Almalki <iiata767@gmail.com>
2023-05-05 13:17:14 +0300

    Added logo

--- client/src/components/Nav/welcomeNav.jsx
+++ client/src/components/Nav/welcomeNav.jsx
@@ -1,5 +1,6 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import React, { Component, Fragment } from "react";
+import Logo from "../../assets/images/newLogo.png";
 
 export class welcomeNav extends Component {
   render() {
@@ -7,7 +8,15 @@ export class welcomeNav extends Component {
       <Fragment>
         <Navbar bg="white" expand="lg" className="pt-2 pb-3 mb-5">
           <Container>
-            <Navbar.Brand>Applier</Navbar.Brand>
+            <Navbar.Brand>
+              <img 
+              src={Logo}
+              height="32"
+              className="d-inline-block align-center" 
+              alt="Owl brand logo" 
+              />{' '}
+              Applier
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav>
