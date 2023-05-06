f32f7ad096660e36f8a0e6a87a093474629b0d46
Turki Almalki <iiata767@gmail.com>
2023-05-06 19:02:20 +0300

    updated home page

--- client/src/components/homePage/services.jsx
+++ client/src/components/homePage/services.jsx
@@ -1,9 +1,9 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import "../../assets/css/welcoming.css";
-import Service1 from "../../assets/images/Services1.jpeg";
-import Service2 from "../../assets/images/Services2.png";
-import Service3 from "../../assets/images/Services3.png";
-import Service4 from "../../assets/images/Services4.jpeg";
+import Service1 from "../../assets/images/service1.jpg";
+import Service2 from "../../assets/images/service3.jpg";
+import Service3 from "../../assets/images/service2.jpg";
+import Service4 from "../../assets/images/service4.jpg";
 import React, { Component, Fragment } from "react";
 
 export class services extends Component {
@@ -11,39 +11,55 @@ export class services extends Component {
     return (
       <Fragment>
         <Container className="container-services" id="Services">
-          <h1 style={{lineHeight: 3}} className="centerAlignment">Our Services</h1>
+          <h1 style={{lineHeight: 3, color: '#4c395d'}} className="centerAlignment">Our Services</h1>
           <Row>
             <Col xl={3} md={6} sm={12}>
-              <div className="card" id="card1">
-                <img src={Service1} alt="Service4" className="ServicesImgs" />
-                <div class="card-body">
-                  <p>Get the right trainee</p>
-                </div>
-              </div>
+              <Card>
+                <Card.Img src={Service1}/>
+              </Card>
+              <h5 style={{
+                fontFamily: "Roboto", 
+                color: '#4c395d', 
+                textAlign: "center", 
+                fontWeight: '700'}}>
+                Get the Right Trainee
+                </h5>
             </Col>
             <Col xl={3} md={6} sm={12}>
-              <div className="card" id="card2">
-                <img src={Service3} alt="Service4" className="ServicesImgs" />
-                <div class="card-body">
-                  <p>Promote opportunities</p>
-                </div>
-              </div>
+              <Card>
+                <Card.Img src={Service3}/>
+              </Card>
+              <h5 style={{
+                fontFamily: "Roboto", 
+                color: '#4c395d', 
+                textAlign: "center", 
+                fontWeight: '700'}}>
+                Promote Opportunities
+                </h5>
             </Col>
             <Col xl={3} md={6} sm={12}>
-              <div className="card" id="card3">
-                <img src={Service2} alt="Service4" className="ServicesImgs" />
-                <div class="card-body">
-                  <p>Obtain an interview for you</p>
-                </div>
-              </div>
+              <Card>
+                <Card.Img src={Service2}/>
+              </Card>
+              <h5 style={{
+                fontFamily: "Roboto", 
+                color: '#4c395d', 
+                textAlign: "center", 
+                fontWeight: '700'}}>
+                Get Interviews
+                </h5>
             </Col>
             <Col xl={3} md={6} sm={12}>
-              <div className="card" id="card4">
-                <img src={Service4} alt="Service4" className="ServicesImgs" />
-                <div class="card-body">
-                  <p>Share your skills</p>
-                </div>
-              </div>
+              <Card>
+                <Card.Img src={Service4}/>
+              </Card>
+              <h5 style={{
+                fontFamily: "Roboto", 
+                color: '#4c395d', 
+                textAlign: "center", 
+                fontWeight: '700'}}>
+                Share Your Skills
+                </h5>
             </Col>
           </Row>
         </Container>
